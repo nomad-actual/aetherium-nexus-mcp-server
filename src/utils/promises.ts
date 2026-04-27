091618b1188a39c107845219d5af1644b19297dd
dlr <centuriond045@gmail.com>
2026-04-26 20:53:38 -0700

    small promise fix

--- src/utils/promises.ts
+++ src/utils/promises.ts
@@ -3,8 +3,6 @@ export async function timeout(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-// abort promise
-
 export async function abort<T>(fn: Promise<T>, signal: AbortSignal, abortReason: string) {
     const _signal = signal
     
@@ -14,11 +12,11 @@ export async function abort<T>(fn: Promise<T>, signal: AbortSignal, abortReason:
             return
         }
 
-        fn.then(res).catch(rej)
-
         _signal.addEventListener('abort', () => {
             rej(abortReason || _signal.reason || 'Abort signal received.')
         })
+
+        fn.then(res).catch(rej)
     })
 }
 
